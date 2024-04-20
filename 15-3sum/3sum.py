37d62b53a72f503ca289e70aca6abcd6e8a3fa76
Shiva <linga.sivaramakrishna@gmail.com>
2024-04-20 15:32:33 -0500

    Time: 1032 ms (30.24%) | Memory: 20.5 MB (77.59%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -14,12 +14,12 @@ class Solution:
                 sum = first_num + second_num + third_num
                 if sum > 0:
                     k -= 1
-                    # while j < k and nums[k] == nums[k + 1]:
-                    #     k -= 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
                 elif sum < 0:
                     j += 1
-                    # while j < k and nums[j] == nums[j - 1]:
-                    #     j += 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
                 else:
                     final_res.add((first_num, second_num, third_num))
                     j, k = j + 1, k - 1
