520b6e5660c03d0cd1d648ccb273b4c50133f9d1
Shiva <linga.sivaramakrishna@gmail.com>
2024-04-20 18:00:36 -0500

    Time: 541 ms (91.85%) | Memory: 20.8 MB (26.63%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,42 +1,44 @@
-from collections import defaultdict
+from itertools import combinations
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        
         res = set()
 
-        #1. Split nums into three lists: negative numbers, positive numbers, and zeros
+        # 1. Split nums into three lists: negative numbers, positive numbers, and zeros
         n, p, z = [], [], []
         for num in nums:
-            if num > 0: p.append(num)
-            elif num < 0: n.append(num)
-            else: z.append(num)
+            if num > 0:
+                p.append(num)
+            elif num < 0:
+                n.append(num)
+            else:
+                z.append(num)
 
-        #2. Create a separate set for negatives and positives for O(1) look-up times
+        # 2. Create a separate set for negatives and positives for O(1) look-up times
         N, P = set(n), set(p)
 
-        #3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
-        #   i.e. (-3, 0, 3) = 0
+        # 3. if triplet contains 0
         if z:
+            if len(z) >= 3:
+                res.add((0, 0, 0))
             for num in P:
                 if -num in N:
                     res.add((-num, 0, num))
 
-        #3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
-        if len(z) >= 3:
-            res.add((0,0,0))
-
-        #4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
+        # 4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
         #   exists in the positive number set
-        for i in range(len(n)):
-            for j in range(i+1,len(n)):
-                target = -(n[i] + n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
+        for x, y in combinations(n, 2):
+            target = -(x + y)
+            if -(x + y) in P:
+                res.add(tuple(sorted([x, y, target])))
 
-        #5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
+        # 5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
         #   exists in the negative number set
-        for i in range(len(p)):
-            for j in range(i+1,len(p)):
-                target = -(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
-        return res
+        for x, y in combinations(p, 2):
+            target = -(x + y)
+            if target in N:
+                res.add(tuple(sorted([x, y, target])))
+
+        return list(res)
