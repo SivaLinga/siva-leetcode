cf6e4385d3a8f03cd88b387dba39e672033bd4a5
Shiva <linga.sivaramakrishna@gmail.com>
2024-04-20 22:52:48 -0500

    Time: 344 ms (5.02%) | Memory: 21.1 MB (73.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,17 @@
+from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        countMap = {}
-        for num in nums:
-            if num in countMap:
-                countMap[num] += 1
-            else:
-                countMap[num] = 1
-        
-        count_in_order = sorted(countMap.values(), reverse = True)
-        
         final_res = []
-        for curr_count in count_in_order:
-            if len(final_res) == k:
-                    return final_res
-            for elem, v in countMap.items():
-                if len(final_res) < k and v == curr_count and elem not in final_res:
-                   final_res.append(elem)
+        countMap = defaultdict(int)
+        for num in nums:
+            countMap[num] += 1
+        
+        count_in_order = sorted(countMap.values())
+
+        while(len(final_res) < k):
+            curr_count = count_in_order.pop()
+            for elem, cnt in countMap.items():
+                if curr_count == cnt:
+                    if len(final_res) < k and elem not in final_res:
+                        final_res.append(elem)
         return final_res
