eac5b7742e195c269988bba5e9de4d70bac549b1
Shiva <linga.sivaramakrishna@gmail.com>
2024-04-27 14:58:26 -0500

    Time: 93 ms (36.49%) | Memory: 22.1 MB (14.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,25 @@
 from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        final_res = []
+        #populate hashmap
         countMap = defaultdict(int)
         for num in nums:
             countMap[num] += 1
+                
+        # create list of buckets
+        bucket = []
+        for i in range(len(nums)):
+            bucket.append([])
         
-        count_in_order = sorted(countMap.values())
-
-        while(len(final_res) < k):
-            curr_count = count_in_order.pop()
-            for elem, cnt in countMap.items():
-                if curr_count == cnt:
-                    if len(final_res) < k and elem not in final_res:
-                        final_res.append(elem)
+        # populate buckets
+        for c_k, c_v in countMap.items():
+            bucket[c_v - 1].append(c_k)
+        
+        final_res = []
+        # loop through each list in the bucket
+        for b1 in reversed(bucket):
+            if len(b1) == 0: continue
+            for b2 in b1:
+                if len(final_res) < k and b2 not in final_res:
+                    final_res.append(b2)
         return final_res
