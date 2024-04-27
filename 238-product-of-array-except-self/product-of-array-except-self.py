ded1cddc2ff7cf45319bd0ea00a14588df570dc7
Shiva <linga.sivaramakrishna@gmail.com>
2024-04-27 17:52:59 -0500

    Time: 264 ms (32.19%) | Memory: 25.6 MB (40.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,23 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # check if there is 0 in the input
-        zero_elem_index = -1
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                zero_elem_index = i
-        
-        prod_result = 1
-        answer = []
-        if zero_elem_index > -1:
-            answer = [0] * len(nums)
-            for idx, num in enumerate(nums):
-                if idx == zero_elem_index: continue
-                prod_result = num * prod_result
-            answer[zero_elem_index] = prod_result
-        else:
-            answer = [nums[0]] * len(nums)
-            for num in nums:
-                prod_result = num * prod_result
-            for i in range(len(nums)):
-                answer[i] = prod_result // nums[i]
+        n = len(nums)
+        answer = [1] * n
+
+        prefix_val = 1
+        for i in range(n):
+            answer[i] = prefix_val
+            prefix_val *= nums[i]
+
+        postfix_val = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= postfix_val
+            postfix_val *= nums[i]
         return answer
