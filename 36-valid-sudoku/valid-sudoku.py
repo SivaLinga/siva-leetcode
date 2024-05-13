64e5371f63b1d65ffade5833f0a613ea47328473
Shiva <linga.sivaramakrishna@gmail.com>
2024-05-12 23:09:44 -0500

    Time: 89 ms (73.58%) | Memory: 16.5 MB (63.38%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,44 +1,20 @@
+from collections import defaultdict
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        #condition1:
-        for row_index, row in enumerate(board):
-            row_map = {}
-            for cell in row:
-                if cell == ".": continue
-                if cell in row_map:
+        rows = defaultdict(set) # key = row_number
+        cols = defaultdict(set) # key = col_number
+        squares = defaultdict(set) # key = (row_number, col_number)
+
+        for row in range(9):
+            for col in range(9):
+                cell = board[row][col]
+                if cell == ".":
+                    continue
+                if (cell in rows[row] or cell in cols[col] or cell in squares[(row // 3, col // 3)]):
                     return False
-                else:
-                    row_map[cell] = 1
-
-        # condition2:
-        for y in range(9):
-            column_map = {}
-            for x in range(9):
-                if board[x][y] == ".": continue
-                if board[x][y] in column_map:
-                    return False
-                else:
-                    column_map[board[x][y]] = 1
-        # condition 3:
-        x1 = 0
-        y1 = 0
-
-
-        while(x1 < 9 or y1 < 9):
-            sub_grid_map = {}
-            for x2 in range(x1, x1+3):
-                for y2 in range(y1, y1+3):
-                    print(x2,y2)
-                    if board[x2][y2] == ".": continue
-                    if board[x2][y2] in sub_grid_map:
-                        return False
-                    else:
-                        sub_grid_map[board[x2][y2]] = 1
-            if x2 == 8 and y2 == 8:
-                break
-            elif y2 == 8:
-                y1 = 0
-                x1 = x1 + 3
-            else:
-                y1 = y1 + 3
+                rows[row].add(board[row][col])
+                cols[col].add(board[row][col])
+                squares[(row // 3, col // 3)].add(board[row][col])
         return True
+
